bc9c918be2800716a957bfd818bcb19a895967af
nayon117 <hasibul.nayon1@gmail.com>
2026-09-04 06:40:15 +0600

    get employee data from db

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm
+from tasks.models import Employee
 
 # Create your views here.
 def manager_dashboard(request):
@@ -15,4 +17,7 @@ def test(request):
     return render(request, 'test.html', context)
 
 def create_task(request):
-    return render(request, 'task_form.html')
+    employees = Employee.objects.all()
+    form = TaskForm(employees=employees)
+    context = {"form": form}
+    return render(request, 'task_form.html', context)
