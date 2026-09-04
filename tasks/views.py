0d2ba94d0c93c1d20bf6fb60585f6461f2b0913f
nayon117 <hasibul.nayon1@gmail.com>
2026-09-04 07:24:20 +0600

    send taskdata to db

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task
 
 # Create your views here.
 def manager_dashboard(request):
@@ -18,6 +18,28 @@ def test(request):
 
 def create_task(request):
     employees = Employee.objects.all()
-    form = TaskForm(employees=employees)
+    form = TaskForm(employees=employees) #for GET
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, employees=employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to')
+
+            task = Task.objects.create(
+                title=title,
+                description=description,
+                due_date=due_date
+            )
+            # assign employee to task
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+
+            return HttpResponse("Task created successfully!")
+
     context = {"form": form}
     return render(request, 'task_form.html', context)
