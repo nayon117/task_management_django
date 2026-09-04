a41855cee69f296086693fb3cbfab99eda949b63
nayon117 <hasibul.nayon1@gmail.com>
2026-09-04 15:10:19 +0600

    apply mixin

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,29 +1,38 @@
 from django import forms
 from tasks.models import Task
 
+class StyledFormMixin:
+    default_classes = 'w-full p-3 border border-gray-300 rounded-lg'
+    def apply_styled_widgets(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f'Enter {field.label.lower()}',
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f'Enter {field.label.lower()}',
+                    'rows': 4,
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class': 'p-2 border border-gray-300 rounded-lg',
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class': 'space-y-2',
+                })
+
+
+
 # Django model form
-class TaskModelForm(forms.ModelForm):
+class TaskModelForm(StyledFormMixin, forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'assigned_to']
-        widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'w-full p-3 border border-gray-300 rounded-lg',
-                'placeholder': 'Enter task title',
-            }),
 
-            'description': forms.Textarea(attrs={
-                'class': 'w-full p-3 border border-gray-300 rounded-lg',
-                'placeholder': 'Enter task description',
-                'rows': 4,
-            }),
-
-            'due_date': forms.SelectDateWidget(attrs={
-                'class': 'p-2 border border-gray-300 rounded-lg',
-            }),
-
-            'assigned_to': forms.CheckboxSelectMultiple(attrs={
-                'class': 'space-y-2',
-            }),
-        }
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
