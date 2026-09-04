2015c867557b1b0ce878e934db5e739896f1b616
nayon117 <hasibul.nayon1@gmail.com>
2026-09-04 14:44:24 +0600

    design form using tailwind

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,13 +1,29 @@
 from django import forms
+from tasks.models import Task
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=250, label='Title')
-    description = forms.CharField(widget=forms.Textarea, label='Description')
-    due_date = forms.DateField(widget=forms.SelectDateWidget, label='Due Date')
-    assigned_to = forms.MultipleChoiceField(widget = forms.CheckboxSelectMultiple, choices = [], label='Assign To')
+# Django model form
+class TaskModelForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'assigned_to']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full p-3 border border-gray-300 rounded-lg',
+                'placeholder': 'Enter task title',
+            }),
 
-    def __init__(self, *args, **kwargs):
-        employees = kwargs.pop('employees', [])
-        print("Employees in form init:", employees)  # Debugging line
-        super().__init__(*args, **kwargs)
-        self.fields['assigned_to'].choices = [(employee.id, employee.name) for employee in employees]
+            'description': forms.Textarea(attrs={
+                'class': 'w-full p-3 border border-gray-300 rounded-lg',
+                'placeholder': 'Enter task description',
+                'rows': 4,
+            }),
+
+            'due_date': forms.SelectDateWidget(attrs={
+                'class': 'p-2 border border-gray-300 rounded-lg',
+            }),
+
+            'assigned_to': forms.CheckboxSelectMultiple(attrs={
+                'class': 'space-y-2',
+            }),
+        }
+
--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,8 @@ from django.db.models import CASCADE
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.name
 
 
 # project model
