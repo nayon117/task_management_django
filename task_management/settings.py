ac029147a8bc811f53c0176f6a35fe8a9d55cd21
nayon117 <hasibul.nayon1@gmail.com>
2026-09-13 18:14:17 +0600

    setup db

--- task_management/settings.py
+++ task_management/settings.py
@@ -92,16 +92,25 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 #     }
 # }
 # For postgresql
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'task_management',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://task_management_utkr_user:XkgYDaUDxqqjmMiIj8dpFms0TgrnDx6y@dpg-daj94krm8hqs73fcli3g-a.oregon-postgres.render.com/task_management_utkr',
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'task_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
