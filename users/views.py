2c44ebc4d1999ec72401e674d24f468a89d9862e
nayon117 <hasibul.nayon1@gmail.com>
2026-09-16 18:33:33 +0600

    create signup form using djangoform

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print(form.cleaned_data)
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
